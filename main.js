fcbbba13338c71f8be7d269b2aec378c3848f57f
Braydon <99298723+BraydonTGS@users.noreply.github.com>
2022-02-22 16:18:16 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let registeredEarly = !true;
+let runnerAge = 32;
+
+if (registeredEarly && runnerAge > 18) {
+    raceNumber += 1000;
+};
+
+if (registeredEarly && runnerAge > 18) {
+    console.log(`You will race at 9:30am and your race number is ${raceNumber}`);
+
+} else if (!registeredEarly && runnerAge > 18) {
+    console.log(`You will race at 11:00am and your race number is ${raceNumber}`);
+
+} else if (runnerAge < 18) {
+    console.log(`You will race at 12:30 and your race number is ${raceNumber}`);
+
+} else {
+    console.log(`See registration!!!`);
+
+};
+
+
+
